86fb5f4fd832d4b1da859dd29b7ae6a5dfd1f687
fmeef <gnu3ra@riseup.net>
2024-09-21 21:44:07 -0500

    add mirroring for ipv4

---
+++ src/api/mirror.rs
@@ -0,0 +1,22 @@
+pub use std::net::{Ipv4Addr, Ipv6Addr};
+
+use flutter_rust_bridge::frb;
+
+#[frb(mirror(Ipv4Addr))]
+#[derive(Clone, Debug)]
+pub struct _Ipv4Addr {
+    octets: [u8; 4],
+}
+
+#[frb(mirror(Ipv6Addr))]
+#[derive(Clone, Debug)]
+pub struct _Ipv6Addr {
+    octets: [u8; 16],
+}
+
+#[frb(mirror(IpAddr, Ipv4Addr, Ipv6Addr))]
+#[derive(Clone, Debug)]
+pub enum _IpAddr {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,5 +1,7 @@
 pub mod connection;
 pub mod error;
 pub mod mdns;
+#[cfg(feature = "flutter")]
+pub mod mirror;
 pub mod response;
 pub mod serialize;
