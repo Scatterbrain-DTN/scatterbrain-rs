554c317640ae074b80cbcca72959748208e9c78c
fmeef <gnu3ra@riseup.net>
2024-09-22 03:40:06 -0500

    workaround using mirroring

--- src/api/mirror.rs
+++ src/api/mirror.rs
@@ -1,22 +1,40 @@
-pub use std::net::{Ipv4Addr, Ipv6Addr};
+pub use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 use flutter_rust_bridge::frb;
 
 #[frb(mirror(Ipv4Addr))]
 #[derive(Clone, Debug)]
 pub struct _Ipv4Addr {
-    pub octets: [u8; 4],
+    octets: [u8; 4],
 }
 
 #[frb(mirror(Ipv6Addr))]
 #[derive(Clone, Debug)]
 pub struct _Ipv6Addr {
-    pub octets: [u8; 16],
+    octets: [u8; 16],
 }
 
-#[frb(mirror(IpAddr, Ipv4Addr, Ipv6Addr))]
+#[frb(mirror(IpAddr, Ipv6Addr, Ipv4Addr))]
 #[derive(Clone, Debug)]
 pub enum _IpAddr {
     V4(_Ipv4Addr),
     V6(_Ipv6Addr),
+}
+
+#[frb(external)]
+impl IpAddr {
+    pub const fn is_unspecified(&self) -> bool {}
+
+    pub const fn is_loopback(&self) -> bool {}
+
+    pub const fn is_multicast(&self) -> bool {}
+
+    pub const fn is_ipv4(&self) -> bool {}
+
+    pub const fn is_ipv6(&self) -> bool {}
+
+    pub const fn to_canonical(&self) -> IpAddr {}
+
+    #[frb(sync)]
+    pub fn to_string(&self) -> String {}
 }
