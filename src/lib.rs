2b091d5af12db7fafbf15738fb38b962bbaa6604
fmeef <gnu3ra@riseup.net>
2024-12-15 02:27:16 -0600

    bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(allocator_api)]
 #[cfg(feature = "flutter")]
 mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 use error::SbResult;
